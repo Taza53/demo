5afb5318911f54f0d5afadd3725c41d0236b27e2
Taza53 <smurtaza.53@outlook.com>
2022-10-06 15:29:16 +0200

    var

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#![allow(unused_variables)]
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    const EARTH_RADIUS_IN_KILOMETERS: f64 = 6371.0;
+
+    let kcle_latitude_degrees: f64 = 41.4075;
+    let kcle_longitude_degrees: f64 = -81.851111;
+
+    let kslc_latitude_degrees: f64 = 40.7861;
+    let kslc_longitude_degrees: f64 = -111.9822;
+
+    
+
+
+
 }
